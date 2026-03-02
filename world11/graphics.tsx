f7a53fe4c1ca2ececce20177cca6b0d15f3ed678
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2026-03-02 22:22:53 +0000

    refactor folder structure

--- world11/graphics.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.03.23" name="graphics" tilewidth="8" tileheight="8" tilecount="256" columns="16">
- <image source="graphics.bmp" width="128" height="128"/>
-</tileset>
